83082a2af7997ce2a70c90cbade1d5ac08cf339c
ravendsgn <bakhtiyorovfq@gmail.com>
2024-12-21 19:24:33 +0500

    Initial commit

---
+++ git/M17.py
@@ -0,0 +1,10 @@
+def func17(b,n,numbers):
+    print(f"b = {b}")
+    print("Haqiqiy sonlar: ")
+    for i in numbers:
+        print(i)
+b = 3.14
+n = 5
+numbers = [1.1, 2.2, 3.3, 4.4, 5.5]
+
+func17(b,n,numbers)
